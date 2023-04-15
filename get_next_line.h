0777f9feb03e76b43f81f41827b18f73744f5d67
Alessio Longo <alessiolongo@192.168.1.109>
2023-04-15 19:51:19 +0200

    add

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: alessiolongo <alessiolongo@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 13:41:27 by mlongo            #+#    #+#             */
-/*   Updated: 2023/04/15 16:41:43 by alessiolong      ###   ########.fr       */
+/*   Updated: 2023/04/15 18:41:44 by alessiolong      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@
 # include <stdlib.h>
 # include <string.h>
 
-void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dst, const void *src, int n);
 void	*ft_calloc(int count, int size);
 char	*get_next_line(int fd);
 char	*ft_substr(char *s, int start, int len);
@@ -26,6 +27,6 @@ int		ft_strchr(char *s, int c);
 int		ft_strlen(char *str);
 char	*ft_strdup(char *s);
 char	*read_buf(int fd, char *buf);
-void	*ft_memset(void *s, int c, size_t n);
+void	*ft_memset(char *s, int c, int n);
 
 #endif
